b89b7f5510a55a66f1225cd5821e26f28276b72a
MikaelDv <enzomikael@hotmail.com>
2024-03-09 18:48:34 -0300

    treinando coding.

---
+++ praticando/adivinhe-o-num.py
@@ -0,0 +1,37 @@
+import random as rdm
+print("/*******BEM-VINDO AO JOGO DO NÚMERO SECRETO*******/")
+
+def main():
+    possibilidade = []
+    dificuldade = int(input("De 1 a 1000, qual o nível de dificuldade desejado?\n"))
+
+    while(dificuldade != 0):
+        possibilidade.append(dificuldade)
+        dificuldade -= 1
+
+    num_secreto = rdm.choice(possibilidade)
+    
+    tentativa = 0
+
+    tentativas = 0
+
+    while(num_secreto != tentativa):
+        pergunta = int(input("Qual é o número secreto? \n"))
+        tentou = tentativa + pergunta
+        tentativas += 1
+        if(num_secreto >  tentou):
+            print(f"O número secreto é maior que {tentou}!")
+        elif(num_secreto < tentou):
+            print(f"O número secreto é menor que {tentou}!")
+        else:
+            print(f"**Você acertou o número secreto, {num_secreto}!**")
+            print(f"**********Com {tentativas} tentativas!**********")
+            break
+
+main()
+
+jogar_nvmt = input("Deseja jogar novamente? (Y/n) ")
+
+if(jogar_nvmt == "y"):
+    main()
+
---
+++ praticando/vol-cilindro.py
@@ -0,0 +1,28 @@
+raio = int(input("Qual é o raio da base do cilindro? "))
+
+altura = int(input("Qual é a altura do cilindro? "))
+
+pi = 3.14
+
+volume = ((raio * raio)*pi)*altura
+
+while True:
+    try:
+        medida = int(input("Os dados foram fornecidos em: \n(1) - Milímetros\n(2) - Centímetros\n(3) - Metros\n(4) - Outro\n"))
+        if(medida == 1):
+            print(f"O volume do cilindro é: {volume}mm³!")
+            break
+        elif(medida == 2):
+            print(f"O volume do cilindro é: {volume}cm³!")
+            break
+        elif(medida == 3):
+            print(f"O volume do cilindro é: {volume}m³!")
+            break
+        elif(medida == 4):
+            nova_medida = input("Qual a medida usada? \n")
+            print(f"O volume do cilindro é: {volume} {nova_medida}³!")
+            break
+        else:
+            print("Opção inválida. Tente novamente!")
+    except:
+        print("Opção inválida. Tente novamente!")
