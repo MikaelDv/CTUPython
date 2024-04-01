239ccdefb74807431de2878974812f3dfdd7ad4f
MikaelDv <enzomikael@hotmail.com>
2024-04-01 11:34:35 -0300

    terminar em casa

--- aula09/ex5.py
+++ aula09/ex5.py
@@ -1,9 +1,16 @@
 linha = int(input("Quantas linhas?"))
 espaco = linha - 1
-print("")
+print()
 for l in range(1, linha*2, 2):
-    print(" "*espaco, end="")
-    print("*" * l)
-    espaco-=1
+    if l == 1:
+        print(" "*espaco, end="")
+        print("*")
+        espaco-=1
+    elif l == (linha*2) - 1:
+        print("*" * l)
+    else:
+        print(" "*espaco, end="")
+        print("*" + " "*l + "*")
+        espaco-=1
 print(f'{" "*(linha-2)}| |')
 print(f'{" "*(linha-2)}| |')
