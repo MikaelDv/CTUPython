ec478eb79f0fb5ca98a279c965c407b2d39abd2e
MikaelDv <enzomikael@hotmail.com>
2024-04-02 10:29:51 -0300

    Arvore vazia completa

--- aula09/ex5.py
+++ aula09/ex5.py
@@ -1,16 +1,16 @@
 linha = int(input("Quantas linhas?"))
 espaco = linha - 1
 print()
-for l in range(1, linha*2, 2):
+for l in range(1, linha * 2, 2):
     if l == 1:
         print(" "*espaco, end="")
         print("*")
         espaco-=1
-    elif l == (linha*2) - 1:
-        print("*" * l)
+    elif l == (linha * 2 - 1):
+        print("*" * (linha * 2 - 1))
     else:
-        print(" "*espaco, end="")
-        print("*" + " "*l + "*")
+        print(" " * espaco, end="")
+        print("*" + " "*(l - 2) + "*")
         espaco-=1
-print(f'{" "*(linha-2)}| |')
-print(f'{" "*(linha-2)}| |')
+print(f'{" "*(linha - 2)}| |')
+print(f'{" "*(linha - 2)}| |')
